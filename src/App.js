8708d0bce66d853e7ed360a6f6647dc82c7343b5
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-22 20:50:30 +0530

    Added Header in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a tinder...</h1>
+      <Header/>
     </div>
   );
 }
