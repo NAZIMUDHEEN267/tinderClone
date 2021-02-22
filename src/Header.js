258eb9bbb53e9baadb46a4cf623ca98707b96c46
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-22 21:03:27 +0530

    Added meterial ui icons

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header(){
     return(
         <div className='header'>
            <h1>I am a header</h1>
+            <PersonIcon/>
         </div>
     )
 }
