0b7d4bc9216899b25d432cd928b196b594cde30a
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-22 21:51:08 +0530

    TinderCard Section Started

---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TinderCards.css'
+
+function TinderCards() {
+    return (
+        <div className='TinderCards'>
+            <h1>This is the body part </h1>
+        </div>
+    )
+}
+
+export default TinderCards;
